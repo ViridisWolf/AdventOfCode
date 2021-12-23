ff97e36b909345ea95fefc54af5b73dc134daf12
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-22 21:12:36 -0600

    Add solutions for days 1, 2, and 3.

---
+++ y2021/day1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from .days import read_data
+
+
+def count_increases(part1=False):
+    prev_value = None
+    window = [None]*3
+    win_index = (1, 3)
+    increase = 0
+
+    if part1:
+        # Disable the sliding window for part 1.
+        window = []
+        win_index = (0, 0)
+
+    lines = read_data('day1.data')
+    for line in lines:
+        window = window[win_index[0]: win_index[1]] + [int(line.strip())]
+
+        if None not in window:
+            value = sum(window)
+            if prev_value is None:
+                pass
+            elif value > prev_value:
+                increase += 1
+            prev_value = value
+
+    print(f"Answer for day 1 part {1 if part1 else 2}: {increase}")
+    return increase
+
+
+def main():
+    count_increases(part1=True)
+    count_increases()
---
+++ y2021/day3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import math
+
+from .days import read_data
+
+
+def main():
+    lines = read_data('day3.data')
+
+    bits = [int(x) for x in lines[0].strip()]
+    for count, line in enumerate(lines[1:]):
+        line = line
+        bits = [int(x) + y for x, y in zip(line, bits)]
+    bits = [math.floor(x / len(lines) + 0.5) for x in bits]
+
+    gamma = sum([1 << i for i, x in enumerate(reversed(bits)) if x])
+    epsilon = (~gamma) & ((1 << len(bits)) - 1)
+    power = gamma * epsilon
+    print(f"Answer for day 3 part 1: {power}")
+
+    o2_candidates = lines[:]
+    co2_candidates = lines[:]
+    for index, bit in enumerate(bits):
+        most_common = str(math.floor(sum([int(x[index]) for x in o2_candidates]) / len(o2_candidates) + 0.5))
+        o2_candidates = [line for line in o2_candidates if line[index] == most_common]
+    assert len(o2_candidates) == 1
+    for index, bit in enumerate(bits):
+        most_common = str(math.floor(sum([int(x[index]) for x in co2_candidates]) / len(co2_candidates) + 0.5))
+        co2_candidates = [line for line in co2_candidates if line[index] != most_common]
+        if len(co2_candidates) == 1:
+            break
+    assert len(co2_candidates) == 1
+
+    o2_rating = int(o2_candidates[0], 2)
+    co2_rating = int(co2_candidates[0], 2)
+    print(f"Answer for day 3 part 2: {o2_rating * co2_rating}")
