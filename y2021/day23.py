6a9594d1829051a2b6e4e512bea00bc69ef4214d
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-23 18:56:14 -0600

    Add day 23 part 2 solution.
    It's ugly, but it works.

--- y2021/day23.py
+++ y2021/day23.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
+
 from functools import cache
 
-from .days import read_data
-
-
 DEBUG = False
+A, B, C, D = 'A', 'B', 'C', 'D'
 
 
 def debug(*args):
     if DEBUG:
         print(*args)
 
-# Idea:
-#   - For each amphipod and each possible move for that amphipod, try that move and recurse.
-#   - Keep track of the best found solution so far, and end as soon as current cost exeeds that.
-#   - Must allow returning inf cost (i.e. no solution).
-#   - How to code up the map?
 
-def part1():
+def print_steps(steps):
+    """ Print the steps of the solution in a similar format to how the examples were done. """
+    for step in steps:
+        cost, hall, rooms = step
+        hall = [x if x else '.' for x in hall]
+        rooms = list(rooms)
+        rooms = [[x if x else '.' for x in r] for r in rooms]
+
+        if cost is not None:
+            print(f"\nMoving at a cost of {cost} yields:")
+        print('#'*13)
+        print('#' + ''.join(hall) + '#')
+        for i in range(len(rooms[0])):
+            print(('  #' if i else '###') + '#'.join([x[i] for x in rooms]) + ('#  ' if i else '###'))
+        print('  #########  ')
+
+
+def part2(part=2):
     move_costs = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
     desired_room = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
+    all_rooms = [('r', x, y) for x in range(4) for y in range(4)]
+    all_valid_halls = [('h', x) for x in [0, 1, 3, 5, 7, 9, 10]]
+    all_valid_locations = all_valid_halls + all_rooms
 
     @cache
     def path_distance(starting, ending, hall, rooms):
@@ -34,12 +48,12 @@ def part1():
 
         assert starting[0] == 'h'
         r, b = ending[1:]
-        if b == 1:
-            # Check that the between bed is empty.
-            if rooms[r][0] is not None:
+        if b > 0:
+            # Check that the between beds are empty.
+            if any(x is not None for x in rooms[r][0:b]):
                 debug(f"Path is blocked at {('r', r, 0)}")
                 return
-            distance += 1
+        distance += len(rooms[r][0:b])
         # The room is clear.  Now check that the hall is clear between the room and starting.
         h_end = 2 + (r*2)
         h_start = starting[1]
@@ -55,88 +69,62 @@ def part1():
     @cache
     def is_done(rooms):
         """ Return True if the rooms have the correct amphs in them, else False """
-        if rooms[0].count('A') == 2:
-            if rooms[1].count('B') == 2:
-                if rooms[2].count('C') == 2:
-                    if rooms[3].count('D') == 2:
+        size = len(rooms[0])
+        if rooms[0].count('A') == size:
+            if rooms[1].count('B') == size:
+                if rooms[2].count('C') == size:
+                    if rooms[3].count('D') == size:
                         return True
         return False
-
-
-    ################################################
-    lines = read_data('day23_test.data')
-    for line in lines:
-        if '.' in line:
-            hall = [None]*(line.count('.'))
-
-    hall = tuple([None] * 11)
-
-    # Index zero of each room is the spot near the hall.  Room #0 is the left most.
-    # Real input.
-    rooms = (('C', 'B'),
-             ('B', 'D'),
-             ('D', 'A'),
-             ('A', 'C'))
-    # Test input.
-    hall_test = (None,None,None,None,None,'D',None,None,None,None,None)
-    rooms_test = ((None, 'A'),
-                  ('B', 'B'),
-                  ('C', 'C'),
-                  ('D', 'A'),)
-    #hall = (None,None,None,None,None,None,None,None,None,'A',None)
-    #rooms_test = ((None, 'A'),
-    #              ('B', 'B'),
-    #              ('C', 'C'),
-    #              ('D', 'D'),)
-    #rooms = rooms_test
-    #hall = hall_test
 
     @cache
     def get_shortest_cost(hall, rooms, cost, best_cost=None):
         """
+        Find the lowest cost path by recursively trying every possibility.
 
-        :param hall:
-        :param rooms:
+        :param hall: The hall tuple indicating where amphs are.
+        :param rooms: The rooms tuple of tuple showing where amphs are.
         :param cost: The cost to reach the current state.
         :param best_cost: The cost of the best known path.
-        :return:
+        :return: (best cost, steps)
         """
+
+        # Store the room state, cost, and such.
+        steps = ()    # [(cost, new_hall, new_rooms)]
 
         # Check to see if we should stop.
         if is_done(rooms):
-            return cost
+            return cost, ()
         if best_cost and cost >= best_cost:
-            return None
-
-        all_rooms = [('r', x, y) for x in range(4) for y in range(2)]
-        all_valid_halls = [('h', x) for x in [0,1,3,5,7,9,10]]
-        all_locations = all_valid_halls + all_rooms
+            return None, ()
 
         # Look for moves from all possible locations.
-        for location in all_locations:
+        for location in all_valid_locations:
             if 'h' == location[0]:
                 # Find an amph that can move.
                 if not hall[location[1]]:
                     continue
                 amph = hall[location[1]]
                 # Amph is in hallway
-                debug(f"There is an {amph} amph at {location}")
+                debug(f"There is an {amph} amph at {location}.  Hall: {hall}")
                 # Move it to all possible room locations.
-                for bed in all_rooms:
-                    r, b = bed[1:]
+                for dest in all_rooms:
+                    r, b = dest[1:]
                     if r != desired_room[amph]:
+                        # Don't want to move to that room.
                         continue
                     if rooms[r][b] is not None:
+                        # Destination is not empty.
                         continue
-                    if b == 0 and rooms[r][1] != amph:
-                        # Can't move into the bed 0 if a different amph type is still in the last bed.
+                    if not all(x == amph for x in rooms[r][b+1:]):
+                        # Can't move into a bed if the further beds are not filled with the correct amph type.
                         continue
-                    debug(f"Amph {amph} at {location} wants to move to {bed}")
+                    debug(f"Amph {amph} at {location} wants to move to {dest}")
                     # We found a good bed!  Now see if we can get there.
-                    dist = path_distance(location, bed, hall, rooms)
+                    dist = path_distance(location, dest, hall, rooms)
                     if dist is None:
                         # Breaking since there will only ever be one valid bed destination.
-                        debug(f"Path from {location} to {bed} is blocked.")
+                        debug(f"Path from {location} to {dest} is blocked.")
                         break
                     # We can move!  Do the move and update the state for the next iteration.
                     new_cost = cost + (move_costs[amph] * dist)
@@ -144,13 +132,14 @@ def part1():
                     new_hall[location[1]] = None
                     new_room[b] = amph
                     new_rooms = rooms[:r] + (tuple(new_room),) + rooms[r+1:]
-                    debug(f"New state after {amph} at {location} moves to {bed} - halls: {new_hall}, rooms: {new_rooms}")
-                    possible_best = get_shortest_cost(tuple(new_hall), new_rooms, new_cost, best_cost=best_cost)
-                    debug(f"If amph {amph} at {location} moves to {bed}, then the best cost after that is {possible_best}.")
+                    debug(f"New state after {amph} at {location} moves to {dest} - halls: {new_hall}, rooms: {new_rooms}")
+                    possible_best, more_steps = get_shortest_cost(tuple(new_hall), new_rooms, new_cost, best_cost=best_cost)
+                    debug(f"If amph {amph} at {location} moves to {dest} at a cost of {new_cost-cost}, then the best cost after that is {possible_best}.")
                     if best_cost is not None and (possible_best is None or possible_best >= best_cost):
                         continue
                     # Found a good path.
                     best_cost = possible_best
+                    steps = ((new_cost-cost, new_hall, new_rooms),) + more_steps
 
             elif location[0] == 'r':
                 r, b = location[1:]
@@ -160,51 +149,92 @@ def part1():
                     continue
                 # Check if this amph is at its final place.
                 if r == desired_room[amph]:
-                    if b == 1:
-                        continue
-                    if b == 0 and rooms[r][1] == amph:
+                    if all(x == amph for x in rooms[r][b:]):
+                        # This amph and all others deeper in the room are at their final spots.
                         continue
 
                 # We found an amph that wants to move.  Now check for all possible destinations.
-                if cost==0:
+                if cost == 0:
                     debug(f"Checking if amph {amph} at {location} can move.")
-                for h in all_valid_halls:
-                    if hall[h[1]]:
-                        if cost==0:
-                            debug(f"Can't move to {h} because it is not empty.")
+                for dest in all_valid_halls:
+                    if hall[dest[1]]:
+                        if cost == 0:
+                            debug(f"Can't move to {dest} because it is not empty.")
                         continue
-                    dist = path_distance(location, h, hall, rooms)
+                    dist = path_distance(location, dest, hall, rooms)
                     if dist is None:
-                        if cost==0:
-                            debug(f"Can't move to {h} because the path is blocked.")
+                        if cost == 0:
+                            debug(f"Can't move to {dest} because the path is blocked.")
                         continue
-                    if cost==0:
-                        debug(f"Can move to {h}.")
+                    if cost == 0:
+                        debug(f"Can move to {dest}.")
                     # We can move to the hall!  Do the move and update the state for the next iteration.
                     new_cost = cost + (move_costs[amph] * dist)
                     new_hall, new_room = list(hall), list(rooms[r])
-                    new_hall[h[1]] = amph
+                    new_hall[dest[1]] = amph
+                    new_hall = tuple(new_hall)
                     new_room[b] = None
                     new_rooms = rooms[:r] + (tuple(new_room),) + rooms[r+1:]
                     assert len(new_rooms) == 4, f"Invalid 'new_rooms': {new_rooms}"
-                    possible_best = get_shortest_cost(tuple(new_hall), new_rooms, new_cost, best_cost=best_cost)
+                    possible_best, more_steps = get_shortest_cost(tuple(new_hall), new_rooms, new_cost, best_cost=best_cost)
                     if cost==0:
-                        debug(f"Best solution cost after amph {amph} at {location} moves to {h}: {possible_best}")
+                        debug(f"Best solution cost after amph {amph} at {location} moves to {dest}: {possible_best}")
                     if best_cost is not None and (possible_best is None or possible_best >= best_cost):
                         continue
                     # Found a good path.
                     best_cost = possible_best
+                    steps = ((new_cost-cost, new_hall, new_rooms),) + more_steps
             else:
                 raise AssertionError()
 
         # We've iterated through every possible move, recursively.
-        return best_cost
+        return best_cost, steps
 
-    lowest_cost = get_shortest_cost(hall, rooms, 0, None)
-    print(lowest_cost)
+    ################################################
+    # Puzzle input:
+    #    #############
+    #    #...........#
+    #    ###C#B#D#A###
+    #      #B#D#A#C#
+    #      #########
+    # After part 2 adjustment:
+    #    #############
+    #    #...........#
+    #    ###C#B#D#A###
+    #      #D#C#B#A#
+    #      #D#B#A#C#
+    #      #B#D#A#C#
+    #      #########
 
-    # Too low: 13518
+    # Index zero of each room is the spot near the hall.  Room #0 is the left most.
+    if part == 1:
+        all_rooms = [('r', x, y) for x in range(4) for y in range(2)]
+        all_valid_locations = all_valid_halls + all_rooms
+        hall = tuple([None] * 11)
+        rooms = ((C, B),
+                 (B, D),
+                 (D, A),
+                 (A, C))
+    elif part == 2:
+        hall = tuple([None] * 11)
+        rooms = ((C, D, D, B),
+                 (B, C, B, D),
+                 (D, B, A, A),
+                 (A, A, C, C))
+    elif part == 'test':
+        hall = (A, A, None, D, None, None, None, None, None, A, D)
+        rooms = ((None, None, None, A),
+                 (B, B, B, B),
+                 (C, C, C, C),
+                 (None, None, D, D),)
+
+    # Done setting up, now find the solution.
+    lowest_cost, steps = get_shortest_cost(hall, rooms, 0, None)
+    if DEBUG:
+        print_steps(((None, hall, rooms),) + steps)
+    print(f"Answer for day 23 part {part}: {lowest_cost}")
 
 
 def main():
-    part1()
+    part2(part=1)
+    part2()
