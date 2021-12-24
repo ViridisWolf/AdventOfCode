5b59df7f2ceb96d4f1fa1da6f3d68bd06ac7d33c
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-23 23:02:42 -0600

    Add days 13, 14, 15, 16, 17, and 18.

---
+++ y2021/day15.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+from collections import namedtuple
+
+from .days import read_data
+
+
+def day15(tile_right, tile_down, part):
+    Node = namedtuple("Node", 'x y')
+
+    def repeat_map(risk_map, right, down):
+        """ Add 1 to each risk value, update the nodes, return the map. """
+        new_map = {}
+        for node in risk_map:
+            new_node = Node(node.x + right*map_width, node.y + down*map_height)
+            risk = (risk_map[node] + right + down) - 1
+            risk = (risk % 9) + 1
+            new_map[new_node] = risk
+        return new_map
+
+    def neighbors(node):
+        """ Returns the neighbor nodes. """
+        neighs = []
+        x, y = node.x, node.y
+        if x + 1 < map_width:
+            neighs.append(Node(x+1, y))
+        if x - 1 >= 0:
+            neighs.append(Node(x-1, y))
+        if y + 1 < map_height:
+            neighs.append(Node(x, y+1))
+        if y - 1 >= 0:
+            neighs.append(Node(x, y-1))
+        return neighs
+
+    def h_est(node, goal):
+        """ Return an estimated risk from the node to the goal. """
+        nodes_est = abs(goal.x - node.x) + abs(goal.y - node.y)
+        risk_est = nodes_est * 5    # 5 is the average risk per node.
+        return 1
+        # return risk_est  # This causes incorrect results...
+
+    lines = read_data('day15.data')
+    map_width = len(lines[0])
+    map_height = len(lines)
+
+    risk_map = {}
+    for row, line in enumerate(lines):
+        for col, risk in enumerate(line):
+            node = Node(int(col), int(row))
+            risk_map[node] = int(risk)
+
+    # Resize the map to the desired size.
+    resized_map = {}
+    for shift_right in range(tile_right):
+        for shift_down in range(tile_down):
+            resized_map.update(repeat_map(risk_map, shift_right, shift_down))
+    # Update map stats.
+    risk_map = resized_map
+    map_height *= tile_right
+    map_width *= tile_down
+    start = Node(0, 0)
+    goal = Node(map_width-1, map_height-1)
+
+    # Start of A* search algorithm.
+    came_from = {start: None}  # Dict of which node a was the previous on the best currently known path to the key node.
+    visibles = set([start])    # Which nodes are visible and need to be scanned.
+    cost_from_start = {}  # Value of each item is the so-far best found cost to that node from the start.
+    cost_total_est = {} # Value of each item is the best estimated cost of a path through that node.
+    cost_from_start[start] = 0  # Start has zero risk to get to start.
+    cost_total_est[start] = h_est(start, goal)
+
+    while visibles:
+        current = min(visibles, key=lambda x: cost_total_est[x])
+        if current == goal:
+            # Found our way to the goal.
+            break
+
+        visibles.remove(current)
+        for neigh in neighbors(current):
+            relative_g = cost_from_start[current] + risk_map[neigh]
+            if neigh not in cost_from_start or relative_g < cost_from_start[neigh]:
+                # We found a better path to this node.  Update the stats.
+                cost_from_start[neigh] = relative_g
+                cost_total_est[neigh] = relative_g + h_est(neigh, goal)
+                came_from[neigh] = current
+                # Add it to the list of nodes to be scanned.
+                visibles.add(neigh)
+                # print(f'{current} neigh: {neigh}, g: {cost_from_start[neigh]}, f: {cost_total_est[neigh]}, h: {h_est(neigh, goal)}')
+
+    # We must have found the path.
+    assert current == goal
+    path = [current]
+    while came_from[current] is not None:
+        prev = came_from[current]
+        path = [prev] + path
+        current = prev
+
+    risk = sum([risk_map[n] for n in path[1:]])
+    # Don't include risk of first node because there is no risk in leaving a node.
+    print(f"Answer for day 15 part {part}: {risk}")  # 388, 2819
+    # risk_path = [str(risk_map[x]) for x in path]; print(''.join(risk_path))
+
+
+def main():
+    day15(1, 1, 1)
+    day15(5, 5, 2)
---
+++ y2021/day17.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import math
+
+from .days import read_data
+
+def day17():
+    class Probe:
+        def __init__(self, vel_x, vel_y):
+            self.velocity_x = vel_x
+            self.velocity_y = vel_y
+            self.position_x = 0
+            self.position_y = 0
+            self.max_height = self.position_y
+
+        def step_position(self):
+            """ Step the probe's position forward in time once. """
+            self.position_x += self.velocity_x
+            self.position_y += self.velocity_y
+            self.max_height = max(self.max_height, self.position_y)
+
+            if self.velocity_x >= 1:
+                self.velocity_x -= 1
+            elif self.velocity_x <= -1:
+                self.velocity_x += 1
+            self.velocity_y -= 1
+
+        def hit_target(self, target):
+            """
+            Returns True if the probe is within the target.  If the target _may_ hit the target in the future, returns
+            None.  If the probe has passed the target, return the which direction was too far.
+             """
+            hit = True
+            if self.position_x < target['x']['min']:
+                if self.velocity_x == 0:
+                    hit = 'y'
+                else:
+                    hit = None
+            if self.position_y > target['y']['max']:
+                hit = None
+            if self.position_x > target['x']['max']:
+                hit = 'x'
+            if self.position_y < target['y']['min']:
+                hit = 'y'
+            return hit
+
+    lines = read_data('day17.data')
+    for line in lines:
+        if 'target area: ' in line:
+            x, y = line[13:].split(', ')
+            assert x.startswith('x=') and y.startswith('y=')
+            x_min, x_max = (x[2:].split('..'))
+            y_min, y_max = (y[2:].split('..'))
+            target = {'x': {'min': int(x_min), 'max': int(x_max)},
+                      'y': {'min': int(y_min), 'max': int(y_max)}}
+            assert target['x']['min'] >= 0, "This function only supports the target being to the right."
+
+    # Calculate the possible ranges.  These assume that the target will always be to the right and down.
+    x_min = math.ceil((2*target['x']['min'] + 0.25)**0.5 - 0.5)
+    x_max = target['x']['max']
+    y_min = target['y']['min']
+    y_max = abs(target['y']['min'])
+
+    # Start trying starting conditions.
+    hits = []
+    for x in range(x_min, x_max + 1):
+        for y in range(y_min, y_max + 1):
+            probe = Probe(x, y)
+            while (hit := probe.hit_target(target)) is None:
+                probe.step_position()
+            if hit is True:
+                hits.append((x, y, probe.max_height))
+            elif hit == 'x':
+                # Passed target horizontally before getting down to the target, so increasing vel_y more won't help.
+                break
+
+    highest = max([x[2] for x in hits])
+    print(f"Answer for day 17 part 1:", highest)
+    print(f"Answer for day 17 part 2:", len(hits))
+
+
+def main():
+    day17()
