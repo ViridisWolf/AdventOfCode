a7cd390ef18b55ba5aa84864227480583b9d03fb
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-28 06:15:02 -0600

    Add day 10 solution.

---
+++ y2021/day10.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import statistics
+
+from . import read_data
+
+corrupted_points_table = {')': 3, ']': 57, '}': 1197, '>': 25137}
+missing_points_table = {')': 1, ']': 2, '}': 3, '>': 4}
+opening_to_closing = {'(': ')', '[': ']', '{': '}', '<': '>'}
+closing_to_opening = {v: k for k, v in opening_to_closing.items()}
+
+
+def day10():
+    corrupted_characters = []
+    missing_scores = []
+
+    lines = read_data('day10.data')
+    for line in lines:
+        corrupted = False
+        stack = []
+        for char in line:
+            if char in '([{<':
+                stack.append(char)
+            elif char in ')]}>':
+                expected = opening_to_closing[stack.pop()]
+                if char != expected:
+                    corrupted = True
+                    corrupted_characters.append(char)
+            else:
+                raise AssertionError
+
+        if stack and not corrupted:
+            score = 0
+            # print(f"Incomplete line: {line}, with these unmatched: {stack}")
+            for char in reversed(stack):
+                missing = opening_to_closing[char]
+                score *= 5
+                score += missing_points_table[missing]
+            missing_scores.append(score)
+
+    score = 0
+    for char in corrupted_characters:
+        score += corrupted_points_table[char]
+    print(f"Answer for day 10 part 1: {score}")
+
+    score = statistics.median(missing_scores)
+    print(f"Answer for day 10 part 2: {score}")
+
+
+def main():
+    day10()
