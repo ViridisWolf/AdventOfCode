00138f4471e551de2a5b2d58b81c30f367636b94
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-29 22:41:26 -0600

    Add 2015 day 4 solution.

--- y2015/__init__.py
+++ y2015/__init__.py
@@ -36,7 +36,7 @@ def get_days():
         # 1: day1.main,
         2: day2.main,
         3: day3.main,
-        # 4: day4.main,
+        4: day4.main,
         # 5: day5.main,
         # 6: day6.main,
         # 7: day7.main,
