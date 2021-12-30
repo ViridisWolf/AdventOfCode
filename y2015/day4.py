38f0db35d598a709556dc34d44df6f8620963bc9
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-29 22:41:26 -0600

    Add 2015 day 4 solution.

---
+++ y2015/day4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import hashlib
+
+puzzle_input = 'iwrupvqb'
+
+
+def day4(part=1):
+    # Init.
+    zeros = '00000'
+    if part == 2:
+        zeros = '000000'
+    number = 1
+
+    # Do the calculations.
+    while True:
+        key = f"{puzzle_input}{number}"
+        md5 = hashlib.md5(key.encode('utf8'))
+        if md5.hexdigest().startswith(zeros):
+            break
+        number += 1
+
+    print(f"Answer for 2015 day 4 part {part}: {number}")
+
+
+def main():
+    day4(part=1)
+    day4(part=2)
