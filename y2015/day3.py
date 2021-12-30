d43d02bd82ba2301e1031569c401aa5017c8f805
Nathan Sinclair <thunderwolf89@gmail.com>
2021-12-29 22:03:22 -0600

    Add 2015 day 3 solution.

---
+++ y2015/day3.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from . import read_data
+
+
+def part1():
+    houses = {(0, 0): 1}
+    # Up is +y, right is +x.
+    x, y = 0, 0
+
+    lines = read_data('day3.data')
+    for line in lines:
+        for char in line:
+            if char == 'v':
+                y -= 1
+            elif char == '<':
+                x -= 1
+            elif char == '^':
+                y += 1
+            elif char == '>':
+                x += 1
+            else:
+                raise AssertionError
+
+            houses[(x, y)] = houses.get((x, y), 0) + 1
+
+    print(f"Answer for 2015 day 3 part 1: {len(houses)}")
+
+
+def part2():
+    houses = {(0, 0): 2}
+    # Up is +y, right is +x.
+    location = {0: [0, 0],
+                1: [0, 0]}
+    turn = 0
+
+    lines = read_data('day3.data')
+    for line in lines:
+        for char in line:
+            if char == 'v':
+                location[turn][1] -= 1
+            elif char == '<':
+                location[turn][0] -= 1
+            elif char == '^':
+                location[turn][1] += 1
+            elif char == '>':
+                location[turn][0] += 1
+            else:
+                raise AssertionError
+            turn ^= 1
+            houses[tuple(location[turn])] = houses.get(tuple(location[turn]), 0) + 1
+
+    print(f"Answer for 2015 day 3 part 2: {len(houses)}")
+
+
+def main():
+    part1()
+    part2()
