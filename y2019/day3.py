f8f6e4b8bfd00e54b8122365eec551044cef4962
Nathan Sinclair <thunderwolf89@gmail.com>
2022-02-01 21:28:22 -0600

    Add solution for 2019 day 3.

---
+++ y2019/day3.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from . import read_data
+
+
+def manhattan_distance(p1, p2):
+    """ Return the manhattan distance between two points. """
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def part1():
+    wire_maps = []
+    for line in read_data('day3.data'):
+        wire = set()
+        wire_maps.append(wire)
+        position = (0, 0)
+        for move in line.split(','):
+            direction = move[0]
+            sign = 1 if direction in ['U', 'R'] else -1
+            vertical = direction in ['U', 'D']
+            amount = int(move[1:])
+
+            for _ in range(amount):
+                if vertical:
+                    position = position[0] + sign, position[1]
+                else:
+                    position = position[0], position[1] + sign
+                wire.add(position)
+
+    overlaps = wire_maps[0].intersection(*wire_maps[1:])
+    if (0, 0) in overlaps:
+        overlaps.remove((0, 0))
+    shortest = min(manhattan_distance((0, 0), p) for p in overlaps)
+
+    print(f"Answer for 2019 day 3 part 1: {shortest}")
+
+
+def part2():
+    wire_maps = []
+    for line in read_data('day3.data'):
+        wire = {}
+        wire_maps.append(wire)
+        position = (0, 0)
+        steps = 0
+
+        for move in line.split(','):
+            direction = move[0]
+            sign = 1 if direction in ['U', 'R'] else -1
+            vertical = direction in ['U', 'D']
+            amount = int(move[1:])
+
+            for _ in range(amount):
+                steps += 1
+                if vertical:
+                    position = position[0] + sign, position[1]
+                else:
+                    position = position[0], position[1] + sign
+                wire[position] = steps
+
+    overlaps = [x for x in wire_maps[0].keys() if x in wire_maps[1]]
+    if (0, 0) in overlaps:
+        del overlaps[(0, 0)]
+    shortest = min(manhattan_distance((0, 0), p) for p in overlaps)
+    delay = min(wire_maps[0][k] + wire_maps[1][k] for k in overlaps)
+
+    print(f"Answer for 2019 day 3 part 1: {shortest}")
+    print(f"Answer for 2019 day 3 part 2: {delay}")
+
+
+def main():
+    part2()
