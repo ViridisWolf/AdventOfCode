ef497630efb792720036f3ea6ecdd9a3a55487e5
Nathan Sinclair <thunderwolf89@gmail.com>
2022-11-25 06:26:29 -0600

    Add solution for 2020 day 3

--- y2020/__init__.py
+++ y2020/__init__.py
@@ -35,7 +35,7 @@ def get_days():
     days = {
         1: day1.main,
         2: day2.main,
-        # 3: day3.main,
+        3: day3.main,
         # 4: day4.main,
         # 5: day5.main,
         # 6: day6.main,
---
+++ y2020/day3.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from . import read_data
+
+
+class TreeMap:
+    def __init__(self, pattern):
+        self.map = tuple(pattern)
+        self.width = len(pattern[0])
+        self.height = len(pattern)
+
+    def get_square(self, x, y):
+        # x = x % self.width
+        # y = y % self.height
+        return self.map[y][x]
+
+    def square_is_tree(self, x, y):
+        """ Returns True if the square is a tree, False if it's empty, or None if it's beyond the top/bottom. """
+        if y > self.height:
+            return None
+
+        char = self.get_square(x, y)
+        assert char in '.#'
+        return char == '#'
+
+
+def day(lines):
+    # https://adventofcode.com/2020/day/3
+
+    pattern = []
+    for line in lines:
+        line = tuple(line.strip())
+        pattern.append(line)
+
+    trees = TreeMap(pattern)
+    tree_count = 0
+    delta_x = 3
+    x = 0
+    for y in range(1, trees.height):
+        x = (x + delta_x) % trees.width
+        tree_count += trees.square_is_tree(x, y)
+
+    print(f"Answer for 2020 day 2 part 1: {tree_count}")
+
+    tree_mult = 1
+    for delta_x, delta_y in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        x = 0
+        tree_count = 0
+        for y in range(delta_y, trees.height, delta_y):
+            x = (x + delta_x) % trees.width
+            tree_count += trees.square_is_tree(x, y)
+        tree_mult *= tree_count
+
+    print(f"Answer for 2020 day 2 part 2: {tree_mult}")
+
+
+
+def main():
+    lines = read_data(__name__.rsplit('.')[-1] + '.data')
+    day(lines)
