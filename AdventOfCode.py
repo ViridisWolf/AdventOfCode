53b24824d32652ff63f9b96856f82829b499d992
Nathan Sinclair <thunderwolf89@gmail.com>
2022-11-25 21:24:24 -0600

    Make solution import code dynamic
    This removes the need to have each year and day hardcoded in various places.

--- AdventOfCode.py
+++ AdventOfCode.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
+import importlib
 import pathlib
-import sys
 import time
-
-sys.path.insert(1, pathlib.Path(__name__).parent)
-
-import y2015
-import y2019
-import y2020
-import y2021
 
 
 def runtime(func, args=()):
@@ -27,26 +20,52 @@ def runtime(func, args=()):
     print(f"^^^ ran in {t1-t0:0.3f} seconds ^^^")
 
 
+def get_years():
+    """ Returns a list of available year numbers as strings. """
+    years = []
+    p = pathlib.Path(__file__).parent
+    for year in p.glob('y*/'):
+        year = year.name[1:]
+        if year.isnumeric():
+            years.append(year)
+    assert years, f"No years found!"
+    return years
+
+
+def get_days(year):
+    """ Returns a list of available days as strings of numbers for the specified year. """
+
+    days = []
+    p = pathlib.Path(__file__).parent
+    for day in p.glob(f'y{year}/day*.py'):
+        day = day.name[3:-3]
+        if day.isnumeric():
+            days.append(day)
+    assert days, f"No days found for year {year}!"
+    days.sort(key=lambda x: int(x))
+    return days
+
+
+def get_day_module(year, day):
+    """ Return the module for the specified year and day. """
+    return importlib.import_module(f'y{year}.day{day}')
+
+
 def main(year=None, day=None):
+    if year:
+        years = [year]
+    else:
+        years = get_years()
 
-    years = {
-            2015: y2015,
-            2019: y2019,
-            2020: y2020,
-            2021: y2021,
-            }
+    for year in years:
+        if day:
+            days = [day]
+        else:
+            days = get_days(year)
 
-    # Run each year.
-    if year is not None:
-        years = {year: years[year]}
-    for y in years.values():
-        days = y.get_days()
-
-        # Run each day function for this year.
-        if day is not None:
-            days = {day: days[day]}
-        for d in days.values():
-            runtime(d)
+        for d in days:
+            module = get_day_module(year, d)
+            runtime(module.main)
 
 
 if __name__ == '__main__':
--- y2021/__init__.py
+++ y2021/__init__.py
@@ -2,14 +2,6 @@
 
 from pathlib import Path
 
-__all__ = []
-scripts = Path(Path(__file__).parent).glob('*.py')
-for script in scripts:
-    if script.name == '__init__.py':
-        continue
-    if script.is_file():
-        __all__.append(script.stem)
-
 
 def read_data(filename):
     """
@@ -23,42 +15,4 @@ def read_data(filename):
         lines = f.readlines()
     lines = [line.strip() for line in lines]
     return lines
-
-
-# "import *" uses the __all__ variable.
-from . import *
-
-
-def get_days():
-    """ Return a list of the main functions for each day in this year. """
-
-    days = {
-        1: day1.main,
-        2: day2.main,
-        3: day3.main,
-        4: day4.main,
-        5: day5.main,
-        6: day6.main,
-        7: day7.main,
-        8: day8.main,
-        9: day9.main,
-        10: day10.main,
-        11: day11.main,
-        12: day12.main,
-        13: day13.main,
-        14: day14.main,
-        15: day15.main,
-        16: day16.main,
-        17: day17.main,
-        18: day18.main,
-        19: day19.main,
-        20: day20.main,
-        21: day21.main,
-        22: day22.main,
-        23: day23.main,
-        # 24: day24.main, # Day 24 was solved mostly by hand. The day24.py file exists, but only has some helper code.
-        25: day25.main,
-        }
-    return days
-
 
