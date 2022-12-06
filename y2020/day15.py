16c63b9bc2f833569c410f729307999fcad82644
Nathan Sinclair <thunderwolf89@gmail.com>
2022-12-06 15:39:58 -0600

    Add solution for 2020 day 15

---
+++ y2020/day15.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from AdventOfCode import read_data
+
+
+def day(data, part):
+    """
+    Each turn, the 'spoken' number is the age of the previous spoken number.  Find what number will be spoken at
+    a certain turn.
+    """
+    final_turn = 2020 if part == 1 else 30000000
+    numbers = {int(val): index for index, val in enumerate(data[0].split(','), 1)}
+    spoken_number = numbers.popitem()[0]
+    # print(f"init numbers: {numbers}, last spoken: {spoken_number}")
+
+    for turn in range(len(numbers)+2, final_turn+1):
+        spoken_turn = numbers.get(spoken_number)
+        numbers[spoken_number] = turn - 1
+        if spoken_turn is None:
+            spoken_number = 0
+        else:
+            spoken_number = turn - 1 - spoken_turn
+        # print(f"Turn {turn} - spoken: {spoken_number}")
+
+    print(f"Answer for {__name__[1:5]} day {__name__[9:]} part {part}: {spoken_number}")
+
+
+def main():
+    data = read_data(__file__)
+    day(data, 1)
+    day(data, 2)
