e7b45de791ca19c07223f282088fc2286c6c8311
Nathan Sinclair <thunderwolf89@gmail.com>
2022-12-13 17:25:09 -0600

    Allow the module for each puzzle to return a result that can be checked
    This commit adds a --check command line argument which adds checking
    that the correct result was returned by each puzzle script.  This also
    includes printing of the result to the screen regardless of whether
    --check is used.
    
    This still allows modules to do their own printing of the result if
    checking is not being done.

--- AdventOfCode.py
+++ AdventOfCode.py
@@ -23,18 +23,60 @@ def read_data(caller, filename=None):
     return lines
 
 
-def runtime(func, args=()):
+def run_and_check(module, args):
+    """
+    Runs the main function, prints the results, and checks them against the expected values.
+
+    :param module: The module which has main().
+    :param args: The args object, which must have correct values in .year and .day.
+    :return: The number of answers which did not match the expected value.
+    """
+
+    count_wrong = 0
+
+    results = module.main()
+    if results is None:
+        # Assume that the module is printing the answers itself.
+        assert not args.check, f"{module.__name__} is not setup for result checking!"
+        return count_wrong
+
+    for part, answer in enumerate(results, 1):
+        if type(answer) is str and '\n' in answer:
+            # Make sure that any multiline answer starts on a new line.
+            print(f"Answer for {args.year} day {args.day} part {part}:\n{answer}", end='')
+        else:
+            print(f"Answer for {args.year} day {args.day} part {part}: {answer}", end='')
+        if not args.check:
+            print()
+            continue
+
+        expected_answer = module.expected_answers[part - 1]
+        if answer == expected_answer:
+            print()
+            continue
+
+        count_wrong += 1
+        if type(expected_answer) is str and '\n' in expected_answer:
+            # Make sure that any multiline answer starts on a new line.
+            print(f"\n ^^^ Wrong answer!  Expected:\n{expected_answer}")
+        else:
+            print(f"  <-- Wrong answer!  Expected {expected_answer}.")
+
+    return count_wrong
+
+
+def runtime(module, args=None):
     """
     Display the runtime of the specified function.
-    :param func: function to run.
-    :param args: Args to be passed to that function.
-    :return: None.
+    :param module: The module with main() to call.
+    :return: Return value from run_and_check().
     """
 
     t0 = time.perf_counter()
-    result = func(*args)
+    result = run_and_check(module, args=args)
     t1 = time.perf_counter()
     print(f"^^^ ran in {t1-t0:0.3f} seconds ^^^")
+    return result
 
 
 def get_years():
@@ -69,29 +111,37 @@ def get_day_module(year, day):
     return importlib.import_module(f'y{year}.day{day}')
 
 
-def main(year=None, day=None):
+def main(year=None, day=None, args=None):
+    wrong_count = 0
+
     if year:
         years = [year]
     else:
         years = get_years()
 
     for year in years:
+        args.year = year
         if day:
             days = [day]
         else:
             days = get_days(year)
 
         for d in days:
+            args.day = d
             module = get_day_module(year, d)
-            runtime(module.main)
+            wrong_count += runtime(module, args=args)
+    return wrong_count
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--year', type=int, help="The AoC year.")
     parser.add_argument('--day', type=int, help="The AoC day.")
+    parser.add_argument('--check', action='store_true', help="Check that each result matches expectation.")
     args = parser.parse_args()
 
     t0_all = time.perf_counter()
-    main(args.year, args.day)
+    result = main(args.year, args.day, args)
     print(f"Total time: {time.perf_counter() - t0_all:0.3f} seconds.")
+    if result:
+        print(f"Failure: {result} answers were wrong.")
