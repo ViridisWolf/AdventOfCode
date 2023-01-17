c62a2b949887554b2e4bddbb66f32ee0413b2bb6
Nathan Sinclair <thunderwolf89@gmail.com>
2023-01-17 02:04:03 -0600

    Improve performance of 2022 day 16 solution
    The stack based approach is replaced with a cached recursive method.
    This is both simpler and faster for the part 2 case.  The code for the
    stack method remains and has been improved, and it is 7x faster than
    the recursive method for part 2.  However, the stack method is unused.

--- y2022/day16.py
+++ y2022/day16.py
@@ -42,12 +42,26 @@ def get_paths(current, selectable):
 
 @cache
 def try_all_orders(start_location, available_time, targets):
+    """
+    Try all possible orders to open the valves, and return the highest amount of pressure that can be released.
+
+    :param start_location: The starting location.
+    :param available_time: The amount of time remaining.
+    :param targets: The available target valves, which must have positive flow rate.
+    :return: The highest amount of pressure that can be released.
+    """
+
+    # This breadth-first stack based approach is 7x faster for part 1 compared to the recursive function, but is
+    # 2.3x slower on part 2.
+
     global valves
 
     if not targets:
         return 0
 
-    done_paths = []
+    # done_paths = []
+    best_flow = 0
+
     # Element format: flowed, remaining time, path (where path is a list of valve names and when they were visited)
     current_paths = [[0, available_time, [(start_location, 0)]]]
     while current_paths:
@@ -60,13 +74,9 @@ def try_all_orders(start_location, available_time, targets):
         dest_paths = get_paths(location, targets)
         # Remove already visited locations.
         dest_paths = {k: v for k, v in dest_paths.items() if k not in path}
-        # Remove any valves which have no flow.
-        dest_paths = {k: v for k, v in dest_paths.items() if valves[k]['flow'] > 0}
 
         # Go to each one, open it, and add that to the stack.
         for dest, path_to_dest in dest_paths.items():
-
-            path_to_dest = get_paths(location, targets)[dest]
             # print(dest, path_to_dest)
             if len(path_to_dest) > remaining_time:
                 # No time to reach and open the valve.
@@ -78,7 +88,8 @@ def try_all_orders(start_location, available_time, targets):
             minute = available_time - new_time
             if len(dest_paths) == 1 or new_time <= 1:
                 # This must be the last valve.
-                done_paths.append((new_flowed, 0, tuple(path_info) + ((dest, minute),)))
+                # done_paths.append((new_flowed, 0, tuple(path_info) + ((dest, minute),)))
+                best_flow = max(best_flow, new_flowed)
             else:
                 # Throw it back on the stack.
                 assert len(dest_paths) > 1
@@ -87,12 +98,40 @@ def try_all_orders(start_location, available_time, targets):
                                       new_time,
                                       tuple(path_info) + ((dest, minute),)])
 
-    # We've tried all valve orders now.  Sort to find the best.
-    done_paths.sort()
-    best = done_paths[-1]
-    flowed, remaining_time, path = best
-    # print(f"Best released {flowed} pressure and went in the order of {path}")
-    return flowed
+    return best_flow
+
+
+@cache
+def get_best_flow_recursive(start_location, available_time, targets):
+    """
+    Try all possible orders to open the valves, and return the highest amount of pressure that can be released.
+
+    :param start_location: The starting/current location.
+    :param available_time: The amount of time remaining.
+    :param targets: The available target valves, which must have positive flow rate.
+    :return: The highest amount of pressure that can be released.
+    """
+
+    # This depth-first recursive version is about 7x slower for part 1 than the breadth-first stack, but 2.3x
+    # faster for part 2.
+
+    global valves
+
+    if not targets or available_time <= 1:
+        return 0
+
+    best_flow = 0
+
+    for destination, path in get_paths(start_location, targets).items():
+        new_time = available_time - len(path)
+        if new_time <= 1 or len(targets) == 1:
+            continue
+
+        new_flowed = valves[destination]['flow'] * new_time
+        new_flowed += get_best_flow_recursive(destination, new_time, tuple(t for t in targets if t != destination))
+        best_flow = max(best_flow, new_flowed)
+
+    return best_flow
 
 
 def try_all_splits(start_location, available_time):
@@ -103,7 +142,7 @@ def try_all_splits(start_location, available_time):
     human = []
     elephant = []
     valve_names = [k for k, v in valves.items() if v['flow'] > 0]
-    for n in range(1, len(valve_names) + 1):
+    for n in range(len(valve_names)//4, len(valve_names)//2 + 1):
         # Don't really need to remove redundant combinations here because of caching used later.
         for combo in itertools.combinations(valve_names, n):
             human.append(combo)
@@ -111,8 +150,8 @@ def try_all_splits(start_location, available_time):
 
     best = 0
     for pair1, pair2 in zip(human, elephant):
-        result = try_all_orders(start_location, available_time, pair1)
-        result += try_all_orders(start_location, available_time, pair2)
+        result = get_best_flow_recursive(start_location, available_time, pair1)
+        result += get_best_flow_recursive(start_location, available_time, pair2)
         best = max(best, result)
 
     return best
@@ -133,7 +172,7 @@ def day(data, part):
                          }
 
     if part == 1:
-        flowed = try_all_orders('AA', total_minutes, tuple(valves.keys()))
+        flowed = get_best_flow_recursive('AA', total_minutes, tuple(k for k, v in valves.items() if v['flow'] > 0))
     else:
         flowed = try_all_splits('AA', total_minutes)
 
