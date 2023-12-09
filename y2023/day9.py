dacfa4428252bd8bf43f67d7139f327b308aeef8
Nathan Sinclair <thunderwolf89@gmail.com>
2023-12-09 01:25:24 -0600

    Speedup 2023 day 9 solution
    The main speedup comes from restructuring the code so that the
    derivatives are re-used for both puzzle parts instead of being
    calculated each time.
    
    There are also some documentation improvements.

--- y2023/day9.py
+++ y2023/day9.py
@@ -13,27 +13,62 @@ def derivative(numbers):
     return output
 
 
-def get_constant(history, part):
+# Not used.
+def get_constant_recursive(history, part):
+    """
+    Return the prefix or postfix extrapolation constant for the input history.
+
+    :param history: The history as a list of numbers.
+    :param part: Whether to do the prefix (part=2) or postfix (part=1) calculation.
+    :return: The calculated extrapolation constant.
+    """
+
     if all([x == 0 for x in history]):
         return 0
 
     diff = derivative(history)
     if part == 1:
-        constant = history[-1]
-        constant += get_constant(diff, part)
+        constant = history[-1] + get_constant_recursive(diff, part)
     else:
-        constant = history[0]
-        constant -= get_constant(diff, part)
+        constant = history[0] - get_constant_recursive(diff, part)
     return constant
 
 
+def calc_constants(diffs):
+    """
+    Return the forward and backwards extrapolation constants from the derivative input.
+
+    :param diffs: List of diff lists, including the original history value.
+    :return: 2-tuple of the backwards and forwards extrapolation constants.
+    """
+    pre_constant = 0
+    post_constant = 0
+    for diff in reversed(diffs):
+        pre_constant = diff[-1] + pre_constant
+        post_constant = diff[0] - post_constant
+    return pre_constant, post_constant
+
+
 def both(data):
+    """
+    For all given sets of sensor history values, calculate a forward and backwards extrapolation value.
+    Then sum the forwards and sum the backwards extrapolation values for all sensor histories.
+
+    :param data: The input sensor history values, as a list of strings.
+    :return: 2-tuple of the sum of forward extrapolations and sum of backwards extrapolations.
+    """
     post_constants = []
     pre_constants = []
     for line in data:
         history = [int(x) for x in line.split()]
-        pre_constants.append(get_constant(history, 1))
-        post_constants.append(get_constant(history, 2))
+        diffs = [history]
+        while not all([x == 0 for x in history]):
+            history = derivative(history)
+            diffs.append(history)
+
+        pre,  post = calc_constants(diffs)
+        pre_constants.append(pre)
+        post_constants.append(post)
 
     return sum(pre_constants), sum(post_constants)
 
