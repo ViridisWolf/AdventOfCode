78bca1cbc9c3ca5d58148670b339015c2908e300
Nathan Sinclair <thunderwolf89@gmail.com>
2026-01-04 19:07:15 -0600

    Minor improvements to 2020 day 1
    This adds a speedup for part 1, docstrings, and a more accurate variable
    name.

--- y2020/day1.py
+++ y2020/day1.py
@@ -3,23 +3,31 @@
 from AdventOfCode import read_data
 
 
-def day1(lines):
-    for index, num1 in enumerate(lines):
-        for num2 in lines[index+1:]:
+def day1(numbers):
+    """Find the first two numbers which sum to 2020, then multiply them together."""
+    large = []
+    small = []
+    for num in numbers:
+        # Assumption: Each number is unique.  If there were duplicates, 1010 would need to be added to `large` too.
+        if num < 1010:
+            small.append(num)
+        else:
+            large.append(num)
+
+    for num1 in large:
+        for num2 in small:
             if num1 + num2 == 2020:
-                answer = num1 * num2
-                break
-
-    # print(f"Answer for 2020 day 1 part 1: {answer}")
-    return answer
+                return num1 * num2
+    raise AssertionError
 
 
-def day1_part2(lines):
-    for index, num1 in enumerate(lines):
-        for index2, num2 in enumerate(lines[index+1:]):
+def day1_part2(numbers):
+    """Find the first three numbers which sum to 2020, then multiply them together."""
+    for index, num1 in enumerate(numbers):
+        for index2, num2 in enumerate(numbers[index + 1:]):
             if num1 + num2 > 2020:
                 continue
-            for num3 in lines[index2+1:]:
+            for num3 in numbers[index2 + 1:]:
                 if num1 + num2 + num3 == 2020:
                     answer = num1 * num2 * num3
                     break
